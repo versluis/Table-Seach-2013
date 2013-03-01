656a42513652214e418e30e7116f23f144fd2791
Jay Versluis <versluis@unknowne0f8472818b8.att.net>
2013-03-01 12:25:42 -0500

    Initial Commit

---
+++ TableSeach2013/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableSeach2013
+//
+//  Created by Jay Versluis on 01/03/2013.
+//  Copyright (c) 2013 Jay Versluis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableSeach2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableSeach2013
+//
+//  Created by Jay Versluis on 01/03/2013.
+//  Copyright (c) 2013 Jay Versluis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
